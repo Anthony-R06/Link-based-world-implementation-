1137d0b438fc874951fec485d316f2ca0d875520
Anthony-R06 <arastell@ucsc.edu>
2026-04-18 15:33:50 -0700

    final

--- rules.js
+++ rules.js
@@ -1,38 +1,82 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData.Title);
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); 
     }
 }
 
+let knowsPin = false;
+let hasStarKey = false;
+let hasExitKey = false;
+
 class Location extends Scene {
-    create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
-        
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
-                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+    create(locationKey) {
+        let locationData = this.engine.storyData.Locations[locationKey];
+        this.engine.show(locationData.Body);
+
+        if (locationData.Choices && locationData.Choices.length > 0) {
+            for (let choice of locationData.Choices) {
+                this.engine.addChoice(choice.Text, choice);
             }
         } else {
-            this.engine.addChoice("The end.")
+            this.engine.addChoice("The end.");
         }
     }
 
     handleChoice(choice) {
-        if(choice) {
-            this.engine.show("&gt; "+choice.Text);
-            this.engine.gotoScene(Location, choice.Target);
-        } else {
+        if (!choice) {
             this.engine.gotoScene(End);
+            return;
         }
+
+        this.engine.show("&gt; " + choice.Text);
+
+        if ((choice.Text == "Exit") && !hasExitKey) {
+            this.engine.show("The entrance is locked. There must be a key somewhere.");
+            this.engine.gotoScene(Location, "Lby");
+            return;
+        }
+
+        if (choice.Target == "R1") {
+            knowsPin = true;
+        }
+
+        if (choice.Target == "R2") {
+            if (knowsPin == false) {
+                this.engine.show("The safe is locked. Maybe there is a clue somewhere.");
+                this.engine.gotoScene(Location, "R2");
+                return;
+            } else if (hasStarKey == false) {
+                this.engine.show("You use the room number clue from the 1st floor as the pin and open the safe.");
+                this.engine.show("Inside you find a star-shaped key.");
+                hasStarKey = true;
+                this.engine.gotoScene(Location, "R2");
+                return;
+            }
+        }
+
+        if (choice.Target == "R3") {
+            if (hasStarKey == false) {
+                this.engine.show("The safe has a star-shaped keyhole. You need to find the right key.");
+                this.engine.gotoScene(Location, "R3");
+                return;
+            } else if (hasExitKey == false) {
+                this.engine.show("You unlock the safe with the star-shaped key.");
+                this.engine.show("Inside you find the hotel exit key.");
+                hasExitKey = true;
+                this.engine.gotoScene(Location, "R3");
+                return;
+            }
+        }
+
+        this.engine.gotoScene(Location, choice.Target);
     }
 }
+
 
 class End extends Scene {
     create() {
